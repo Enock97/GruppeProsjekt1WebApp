002402a9a8458c3a9034d8402b9c8cb18e0e91e4
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-17 11:16:42 +0100

    Update index.js
    La til en fail-function i index.js

--- EksamenVersjon3/wwwroot/js/index.js
+++ EksamenVersjon3/wwwroot/js/index.js
@@ -7,6 +7,16 @@ $(function () { //Denne funksjonen er selvutviklet men noen av delene er hentet 
 function hentAlleObservasjoner() { //Denne funksjonen er selvutviklet men noen av delene er hentet fra KundeApp2-med-DAL filen fra canvas
     $.get("observasjon/hentAlle", function (observasjoner) {
         formaterObservasjoner(observasjoner);
+    })
+
+    //Denne koden er hentet fra "wwwroot/js/index.js" mappen som igjen ligger under mappen "KundeApp2-med-logginn-sessions" hentet fra canvas
+    .fail(function (feil) {
+        if (feil.status == 401) {
+            window.location.href = 'loggInn.html'; // ikke logget inn, redirect til loggInn.html
+        }
+        else {
+            $("#feil").html("Feil på server - prøv igjen senere");
+        }
     });
 }
 
