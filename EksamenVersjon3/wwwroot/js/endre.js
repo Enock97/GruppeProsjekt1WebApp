8b471b3808ba5fb28c629869ce9e6331b5aa8300
Nora-bakke <71878743+Nora-bakke@users.noreply.github.com>
2022-11-17 11:16:34 +0100

    Update endre.js
    La til en fail-function i endre.js

--- EksamenVersjon3/wwwroot/js/endre.js
+++ EksamenVersjon3/wwwroot/js/endre.js
@@ -29,5 +29,17 @@ function endreObv() { //Denne funksjonen er selvutviklet men noen av delene er h
         else {
             $("#feil").html("Error: Klarte ikke endre Observasjon");
         }
+    })
+
+     //Denne koden er hentet fra "wwwroot/js/index.js" mappen som igjen ligger under mappen "KundeApp2-med-logginn-sessions" hentet fra canvas
+    .fail(function (feil) {
+        if (feil.status == 401) {  // ikke logget inn, redirect til loggInn.html
+            window.location.href = 'loggInn.html';
+        }
+        else {
+            $("#feil").html("Feil på server - prøv igjen senere");
+        }
     });
+
+
 }
